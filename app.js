c5cadecbf205524943341e2fc06e192f10a462b5
Ignacio Picca <piccaignacio@gmail.com>
2022-11-18 15:23:34 -0300

    time

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function gameStart() {
-
+    let seconds = 0;
+    let time = document.getElementById("time");
+    window.setInterval(function () {
+        time.innerHTML = seconds;
+        seconds++;
+    }, 1000);
 }
+
