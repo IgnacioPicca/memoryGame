00ce3e415e572529f77e7f1ab34da55583efc095
Ignacio Picca <piccaignacio@gmail.com>
2022-11-19 09:48:41 -0300

    estilado y elementos

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
+const elements = {
+    moves: document.querySelector('#moves'),
+    timer: document.querySelector('#time'),
+    start: document.querySelector('btn'),
+
+}
+
+const sets = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    time: 0,
+
+}
+
+
+
+
+
 function gameStart() {
     //Comenzamos a contar los segundos
     startTime();
 }
 
 function startTime() {
-    let seconds = 0;
+    let seconds = 1;
     let time = document.getElementById("time");
     window.setInterval(function () {
-        time.innerHTML = seconds;
+        time.textContent = seconds;
         seconds++;
     }, 1000);
 }
 
-function flipCard() {
-    let aux = 1;
-    let moves = document.getElementsByClassName("moves");
-    moves.innerHTML = aux;
+function countMoves() {
+
+    let card = document.getElementsByClassName("cards");
+    let moves = document.getElementById("moves");
+    let contador = 0;
+
+    card.onclick = function () {
+        contador++;
+        moves.textContent = contador;
+    }
+
 }
+
+let card = document.getElementsByClassName("cards");
+card.onclick = function () {
+    console.log("hola")
+}
+
+
+
